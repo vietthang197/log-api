8b2ea046a1894bab5adcec6b95f66c467e243d9a
Lê Việt Thắng <levietthang1997@gmail.com>
2021-07-15 22:21:49 +0700

    Initial commit

---
+++ src/test/java/com/thanglv/logapi/LogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanglv.logapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
